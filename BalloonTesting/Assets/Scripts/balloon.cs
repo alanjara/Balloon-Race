0efb1c4011505fb8e0731138a4c93f0f47432a21
wjhowell <wjhowell@umich.edu>
2016-02-19 00:00:38 -0500

    First commit woo!
    Hot air balloon+basket model, terrain model, super wonky balloon
    physics. mouse click to elevate, WASD to move balloon

---
+++ BalloonTesting/Assets/Scripts/balloon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balloon : MonoBehaviour {
+	Rigidbody rb;
+	public float verticalThrust;
+	public float horizontalThrust;
+	public bool mouse;
+	public float v, h;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouse = Input.GetMouseButton (0);
+		if (mouse)
+			rb.AddRelativeForce (Vector3.forward * verticalThrust, ForceMode.Acceleration);
+		//else
+			//rb.AddRelativeForce (-Vector3.forward / 10f, ForceMode.Force);
+			//transform.position = Vector3.Lerp (transform.position, transform.position - Vector3.up * .01f, Time.deltaTime);
+		v = Input.GetAxis("Vertical");
+		h = Input.GetAxis("Horizontal");
+
+		if(v > 0) rb.AddRelativeForce (Vector3.up * -horizontalThrust, ForceMode.Acceleration);
+		else if (v < 0) rb.AddRelativeForce (Vector3.up * horizontalThrust, ForceMode.Acceleration);
+		if(h > 0) rb.AddRelativeForce (Vector3.right * horizontalThrust, ForceMode.Acceleration);
+		else if (h < 0) rb.AddRelativeForce (Vector3.right * -horizontalThrust, ForceMode.Acceleration);
+	}
+}
---
+++ BalloonTesting/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+	public GameObject target;
+	public float FollowDistance = 7.5f;
+	public float speed = 2f;
+	public float distThres = .01f;
+	public Vector3 newPos;
+	public static cameraFollow S;
+	void Awake()
+	{
+		S = this;
+	}
+	void Start()
+	{
+		newPos = Camera.main.transform.position;
+		newPos.y = target.transform.position.y + 4f; ;
+		newPos.z = target.transform.position.z - FollowDistance;
+		Camera.main.transform.position = newPos;
+	}
+	void FixedUpdate()
+	{
+		newPos = Camera.main.transform.position;
+		newPos.x = target.transform.position.x;
+		newPos.y = target.transform.position.y + 4f;
+		newPos.z = target.transform.position.z - FollowDistance;
+
+		if (Mathf.Abs(newPos.z - Camera.main.transform.position.z) > distThres)
+		{
+			Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, newPos, Time.deltaTime * speed + Mathf.Abs(target.GetComponent<Rigidbody>().velocity.z) / 30f);
+		}
+		else
+		{
+			Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, newPos, Time.deltaTime * speed);
+		}
+		transform.LookAt (target.transform);
+	}
+
+}
