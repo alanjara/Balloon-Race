bd00f189e6cf7723b85aaf23443e73700760cfe2
wjhowell <wjhowell@umich.edu>
2016-02-23 06:56:28 -0500

    Fixed balloon in mainmenu, buttons load alan's scene

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public Button onePlayer;
+	public Button twoPlayers;
+
+	void Start(){
+		onePlayer.onClick.AddListener(() => loadOnePlayer());
+		twoPlayers.onClick.AddListener(() => loadTwoPlayer());
+	}
+
+	void loadOnePlayer(){
+		SceneManager.LoadScene ("SceneAlan");
+	}
+
+	void loadTwoPlayer(){
+		SceneManager.LoadScene ("SceneAlan");
+	}
+}
