b5b2f4cce47cab050fb3b2dd4f87d8491452388b
Stali Stalev <Stali Stalev>
2016-02-25 17:46:36 -0500

    poofs and rings

---
+++ Assets/ArrowAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowAnimation : MonoBehaviour {
+
+    bool goingup = true;
+    public float offset = 0.3f, speed = 2f;
+    Vector3 uploc, downloc;
+	// Use this for initialization
+	void Start () {
+        uploc = transform.position + Vector3.up * offset;
+        downloc = transform.position - Vector3.up * offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (goingup)
+        {
+            transform.position = Vector3.Lerp(transform.position, uploc, Time.deltaTime * speed);
+            if((transform.position - uploc).magnitude < 0.1)
+            {
+                goingup = false;
+            }
+        } else
+        {
+            transform.position = Vector3.Lerp(transform.position, downloc, Time.deltaTime * speed);
+            if ((transform.position - downloc).magnitude < 0.1)
+            {
+                goingup = true;
+            }
+        }
+	}
+}
---
+++ Assets/Poof.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poof : MonoBehaviour {
+    public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+        targetScale = new Vector3(4f, 1.5f, 4f);
+	}
+
+
+
+    public Vector3 targetScale;
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * speed);
+	}
+}
