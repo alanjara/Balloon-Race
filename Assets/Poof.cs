4d8e7c747a20f7701b8f5ec5caa24dd80ad2ca9c
Stali <sstalev@umich.edu>
2016-02-25 18:13:44 -0500

    Poofs now work

--- Assets/Poof.cs
+++ Assets/Poof.cs
@@ -6,13 +6,25 @@ public class Poof : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         targetScale = new Vector3(4f, 1.5f, 4f);
-	}
+        fading = gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material;
+        StartCoroutine(killyourself());
+    }
+    Material fading;
 
+
+    IEnumerator killyourself()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
 
 
     public Vector3 targetScale;
 	// Update is called once per frame
 	void Update () {
         transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * speed);
+        Color nicememe = fading.GetColor("_Color");
+        nicememe.a -= Time.deltaTime * 0.5f;
+        fading.SetColor("_Color",nicememe);
 	}
 }
