e4645346e454909be21cd68194a5a55f547855a8
wjhowell <wjhowell@umich.edu>
2016-03-09 14:57:35 -0500

    level select mostly finished

---
+++ Assets/Scripts/LevelSelectUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;	
+
+public class LevelSelectUI : MonoBehaviour {
+	public Transform baboonPosition;
+	public Button Level1;
+	public Button Level2;
+	public Button Level3;
+
+	// Use this for initialization
+	void Start () {
+		Level1.onClick.AddListener(() => loadLevel1());
+		Level2.onClick.AddListener(() => loadLevel2());
+		Level3.onClick.AddListener(() => loadLevel3());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (baboonPosition.position.x > .5)
+			Level3.Select();
+		if (baboonPosition.position.x <= .5 && baboonPosition.position.x >= -.5)
+			Level2.Select();
+		if (baboonPosition.position.x < -.5)
+			Level1.Select();
+	}
+		
+	void loadLevel1(){
+		SceneManager.LoadScene ("Scene_Alan");
+	}
+	void loadLevel2(){
+		SceneManager.LoadScene ("Scene_Alan");
+	}
+	void loadLevel3(){
+		SceneManager.LoadScene ("CityAlanLatest");
+	}
+}
