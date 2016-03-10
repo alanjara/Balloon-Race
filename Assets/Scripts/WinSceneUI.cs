cf50adf1f18435bef66ec6856d9e94767a8f10d8
Wesley Howell <wjhowell@umich.edu>
2016-03-09 20:12:29 -0500

    Win Scene

---
+++ Assets/Scripts/WinSceneUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinSceneUI : MonoBehaviour {
+
+	public Button MainMenu;
+	public float speed;
+
+	void Start () {
+		MainMenu.onClick.AddListener(() => loadMainMenu());
+		GameObject.Find("Player_").GetComponent<Text>().text += PersistentGameData.raceWinner;
+	}
+
+	void Update(){
+		Color color = GameObject.Find ("Player_").GetComponent<Text> ().color;
+		color.g = (Mathf.Sin (Time.time * speed) + 1.0f) / 2.0f;
+		color.b = (Mathf.Sin (Time.time * speed) + 1.0f) / 2.0f;
+		GameObject.Find("Player_").GetComponent<Text>().color = color;
+	}
+
+	void loadMainMenu(){
+		SceneManager.LoadScene ("_MainMenu");
+	}
+}
