375222e1c004b9b9be2000ab1d5d31fb7b46b73d
chendifu <chendifu@umich.edu>
2016-03-09 23:26:16 -0500

    added the flame for the volcano level

---
+++ Assets/ThrowFlame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowFlame : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject fire;
+    public const float cooldown = 5f;
+    private float timer;
+    public GameObject flameStriker;
+	void Start () {
+        timer = cooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <=0)
+        {
+            timer = cooldown;
+            Instantiate(fire, transform.position, transform.rotation);
+            Invoke("fireStrike", 0.2f);
+            Invoke("fireStrike", 0.4f);
+            Invoke("fireStrike", 0.6f);
+            Invoke("fireStrike", 0.8f);
+        }
+	}
+
+    void fireStrike()
+    {
+
+        Instantiate(flameStriker, transform.position, transform.rotation);
+    }
+}
